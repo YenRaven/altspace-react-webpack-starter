b2344c7f0ee103c4eb74f199e1f29fc64711e7bf
BEHEMOTH\Richard <yenraven@gmail.com>
2017-03-19 03:13:53 -0500

    simple hello world

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const AppConfig = {
+    fullspace:false,
+    verticalAlign: "middle",
+    syncSystem: false,
+    appName: "New App",
+    author: "Your Name Here"
+};
+
 class Main extends React.Component {
   render() {
-    return <h1>Hello World Of Altspace</h1>;
+    return (
+        <a-scene
+            altspace={`vertical-align: ${AppConfig.verticalAlign}; fullspace: ${AppConfig.fullspace};`}
+            sync-system={AppConfig.syncSystem?`app: ${AppConfig.appName}; author: ${AppConfig.author}` : null}
+        >
+            <a-entity n-text="text: Hello World!;" />
+        </a-scene>
+    )
   }
 }
 
